7c76e92abc07e7bff776a6e2527c439ba6df1ef9
Nabin Khati <nabeenkhati7@gmail.com>
2026-05-13 13:46:57 +0530

    initial commit

---
+++ src/main/java/com/program10/Maven/prog10/ci/App.java
@@ -0,0 +1,13 @@
+package com.program10.Maven.prog10.ci;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
